56559ec1c8ca578262d831183a27bc08978820b6
Rawdawgcon <160992955+Rawdawgcon@users.noreply.github.com>
2024-03-03 20:52:00 -0800

    Create AlertManager.java

---
+++ AlertManager.java
@@ -0,0 +1,7 @@
+public class AlertManager {
+    public static void notify(String message) {
+        // Implement alerting mechanism here
+        // For example, show a notification to the user
+        Log.e("Alert", message);
+    }
+}
